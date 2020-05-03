77b2c3d3f94100cc27eec58059f9aa0822643cc0
unknown <jagamez145@hotmail.com>
2020-05-03 15:32:23 -0500

    Gera Letras

---
+++ src/Dios.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Dios.css';
+class  Dios extends React.Component {
+ render() {
+ return  <div className="container">
+ 
+ <div className="banckground trans"></div>
+ 
+  <div className="background">
+    <div className="text"> Me dice que me ama cuando escucho llover
+    Me dice que me ama Con un atardecer Lo dice sin palabras con las olas del mar
+    Lo dice en la mañana Con mi respirar</div>
+    <div className="text">#Dios_me_dice_que_me_ama_una_y_otra_vez.
+  </div>
+  </div>
+  </div>
+ 
+   
+
+
+
+}
+}
+export default Dios;
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Dios from './Dios';
+
+const container = document.getElementById('root');
+
+ReactDOM.render(<Dios/>, container);
